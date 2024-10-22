62097c9c59219d41d075802921361d548a269567
MJDeC <mjdecaro@students.unwsp.edu>
2024-10-22 18:09:01 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -4,6 +4,18 @@
 # Then ask the user for a subject (like "COS"). 
 # Finally, the program will display the ID and name of all the courses having that subject.
 def main():
+  cont='y'
   course_ids={}
-  course_input=input("Enter a course ID")
-  course_input2=input("Enter the course name that corresponds with the ID.")
+  if cont=='y':
+    cont=input("Enter y to add more course values. Enter another letter key to continue.")
+    course_input1=input("Enter a subject letter tag:")
+    course_input2=input("Enter a course ID number to go with the letters:")
+    course_input3=input("Enter the corresponding course name:")
+    course_ids={course_input1,course_input2,course_input3}
+    cont=input("Enter y to add more course values. Enter another letter key to continue.")
+  else:
+    choose=input("Enter a subject to see the IDs and names for that subject.")
+    course_list=course_ids.get(choose,'No courses with that ID are entered.')
+    print(course_list)
+  
+main()
